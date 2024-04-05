5f67d67684743ca2c92248a28b223b7273991e89
byozdemir <emrenetwork@yandex.com>
2024-04-05 14:57:19 +0300

    DRB Terminal changes

--- drb.js
+++ drb.js
@@ -2,25 +2,47 @@ const path = require("path");
 const fs = require("fs");
 
 const generateModel = (model) => {
-  var gerenatedCode = `class ${model.modelName}(models.Model):\n`;
+  var generatedCode = `\nclass ${model.modelName}(models.Model):\n`;
   model.fields.forEach((field) => {
-    if(field.modelItem==false){
-        return;
+    if (field.modelItem == false) {
+      return;
     }
-    gerenatedCode += `\t${field.fieldName} = models.${field.type}(verbose_name='${field.label}'`;
-    if (field.attrs) {
-      gerenatedCode += ",";
-      field.attrs.forEach((attr, index) => {
+    generatedCode += `\t${field.fieldName} = models.${field.type}(verbose_name='${field.label}'`;
+    if (field.djangoAttrs) {
+      generatedCode += ",";
+      field.djangoAttrs.forEach((attr, index) => {
         const parsed = attr.split("::");
-        gerenatedCode += `${
+        generatedCode += `${
           parsed.length > 1 ? parsed[0] + "(" + parsed[1] + ")" : parsed[0]
-        }${index !== field.attrs.length - 1 ? "," : ""}`;
+        }${index !== field.djangoAttrs.length - 1 ? "," : ""}`;
       });
     }
-    gerenatedCode += ")\n";
+    generatedCode += ")\n";
   });
-  gerenatedCode += `\tclass Meta:\n\t\tverbose_name = '${model.label}'\n\t\tverbose_name_plural = '${model.label}'`;
-  console.log(gerenatedCode);
+  generatedCode += `\n\tdef __str__(self):\n\t\treturn '${model.label}'\n`;
+  generatedCode += `\n\tclass Meta:\n\t\tverbose_name = '${model.label}'\n\t\tverbose_name_plural = '${model.label}'\n\n`;
+  return generatedCode;
+};
+
+const generateSchema = (model) => {
+  var generatedCode = `\nconst ${model.modelName}Schema = yup.object().shape({\n`;
+  model.fields.forEach((field) => {
+    if (field.formItem == false) {
+      return;
+    }
+    generatedCode += `\t${field.fieldName}: yup.${field.frontendType}()\n`;
+    if (field.reactAttrs) {
+      field.reactAttrs.forEach((attr, index) => {
+        const parsed = attr.split("::");
+        generatedCode += `\t.${
+          parsed.length > 1 ? parsed[0] + "(" + parsed[1] + ")" : parsed[0]
+        }${index == field.reactAttrs.length - 1 ? ",\n" : "\n"}`;
+      });
+    }
+    generatedCode += "\n";
+  });
+  generatedCode += "});\n";
+  return generatedCode;
 };
 
 const getAllDrbSchema = (target) => {
@@ -36,14 +58,50 @@ const getAllDrbSchema = (target) => {
   return files;
 };
 
-DRBSCHEMA_PATH = path.resolve(__dirname, "drbschema");
-DRBDJANGO_PATH = path.resolve(__dirname, "src");
-DRBFRONTEND_PATH = path.resolve(DRBDJANGO_PATH, "frontend");
+const DRBSCHEMA_PATH = path.resolve(__dirname, "drbschema");
+const DRBDJANGO_PATH = path.resolve(__dirname, "src");
+const DRBFRONTEND_PATH = path.resolve(DRBDJANGO_PATH, "frontend");
+const SCHEMA_PATH = path.resolve(DRBFRONTEND_PATH, "lib/schemas")
 const files = getAllDrbSchema(DRBSCHEMA_PATH);
 files.forEach((item) => {
-  var modelCode = "";
+  var modelCode = "from django.db import models\n\n";
+  var schemaCode = "import * as yup from 'yup';\n";
   const importedObject = require(path.resolve(DRBSCHEMA_PATH, item));
+  const appPath = path.resolve(
+    DRBDJANGO_PATH,
+    importedObject.DRBObject.django.app
+  );
   importedObject.DRBObject.django.models.forEach((model) => {
     const code = generateModel(model);
+    modelCode += code;
+    schemaCode += generateSchema(model);
+  });
+  fs.writeFile(path.resolve(appPath, "models.py"), modelCode, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(
+        `${
+          importedObject.DRBObject.django.app
+        } Model file successfully created to path:${path.resolve(
+          appPath,
+          "models.py"
+        )}`
+      );
+    }
+  });
+  fs.writeFile(path.resolve(SCHEMA_PATH, importedObject.DRBObject.django.app+'Schema.js'), schemaCode, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(
+        `${
+          importedObject.DRBObject.django.app
+        } Schema file successfully created to path:${path.resolve(
+          SCHEMA_PATH,
+          importedObject.DRBObject.django.app+'Schema.js'
+        )}`
+      );
+    }
   });
 });
--- drbschema/user_object.js
+++ drbschema/user_object.js
@@ -10,26 +10,67 @@ const DRBObject = {
             fieldName: "username",
             label: "User Name",
             type: "CharField",
-            djangoAttrs: ["max_length::20", "required"],
-            reactAttrs:['min::3','max::10','required']
+            frontendType:'string',
+            djangoAttrs: ["max_length=20",],
+            reactAttrs:['min::3','max::10',]
           },
           {
             fieldName: "password",
             label: "Password",
             type: "CharField",
-            djangoAttrs: ["max_length::20"],
-            reactAttrs:['min::8','max::32','required']
+            frontendType:'string',
+            djangoAttrs: ["max_length=20"],
+            reactAttrs:['min::8','max::32',]
           },
           {
             fieldName: "private_key",
             label: "private_key",
             type: "CharField",
+            frontendType:'string',
             formItem:false, // Use this prop if you don`t want to add this field to frontend forms.Default:true
-            djangoAttrs: ["max_length::20"],
+            djangoAttrs: ["max_length=20"],
           },
           {
             fieldName: "repassword",
             label: "Password",
+            frontendType:'string',
+            modelItem: false, //Use this prop if you don`t want to add this field to django model.Default:true
+            reactAttrs:['min::8','max::32','required']
+          },
+        ],
+      },
+      {
+        modelName: "Accounts2",
+        label: "Accounts2",
+        fields: [
+          {
+            fieldName: "username2",
+            label: "User Name",
+            type: "CharField",
+            frontendType:'string',
+            djangoAttrs: ["max_length=20",],
+            reactAttrs:['min::3','max::10',]
+          },
+          {
+            fieldName: "password2",
+            label: "Password",
+            type: "CharField",
+            frontendType:'string',
+            djangoAttrs: ["max_length=20"],
+            reactAttrs:['min::8','max::32',]
+          },
+          {
+            fieldName: "private_key",
+            label: "private_key2",
+            type: "CharField",
+            frontendType:'string',
+            formItem:false, // Use this prop if you don`t want to add this field to frontend forms.Default:true
+            djangoAttrs: ["max_length=20"],
+          },
+          {
+            fieldName: "repassword2",
+            label: "Password",
+            frontendType:'string',
             modelItem: false, //Use this prop if you don`t want to add this field to django model.Default:true
             reactAttrs:['min::8','max::32','required']
           },
